73e912fcb430c8d8f6ec0d337aa976fdbd885eed
signalboss <racehorsetoday@gmail.com>
2025-10-21 19:21:21 -0700

    Disable ESLint during build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false,
   eslint: {
+    // Disable ESLint during build
     ignoreDuringBuilds: true,
   },
 };
